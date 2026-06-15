d141fd92f161f7278cbc4790157a9e7b10816292
abdul-kadir2008 <ka2355504@gmail.com>
2026-06-15 09:56:41 +0530

    Day 2 first Question is done.

---
+++ Core_Java_Practice/src/day_02_/Q3.java
@@ -0,0 +1,26 @@
+//Write a Java program to create a function countDigits(int n) 
+//that returns the total number of digits present in a given number.
+package day_02_;
+import java.util.Scanner;
+public class Q3 {
+	
+public static void main()
+{
+	Scanner sc =new Scanner(System.in);
+	System.out.println("Enter Your Number :");
+int n=sc.nextInt();
+
+System.out.println(countDigits(n));
+	}
+ static int countDigits(int num)
+{
+	int count=0;
+	while(num!=0)
+	{
+		num/=10;
+		count++;
+		
+	}
+	return count;
+}
+}
